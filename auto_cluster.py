3b756140fc7691a408b08caa45848fd1503b8d28
Neal McBurnett <neal@mcburnett.org>
2017-04-25 08:00:17 -0600

    good run of all files

--- auto_cluster.py
+++ auto_cluster.py
@@ -6,6 +6,7 @@ import os
 import codecs
 import re
 import json
+import logging
 import collections
 import numpy as np
 import matplotlib.pyplot as plt
@@ -54,7 +55,7 @@ def findRecordLen(segment, maxLen=1000):
 
     (lags, c, line, b) = plt.acorr(arrNorm, maxlags=maxLen)
     
-    return c[maxLen+1:].argmax() + 1
+    return int(c[maxLen+1:].argmax()) + 1
 
 # ETL
 
@@ -64,15 +65,19 @@ def signatures(fn):
 
     filelen = len(data)
 
-    print("File: %s" % fn)
-    print("File length: %d" % filelen)
+    stat = os.stat(fn)
+    desc = collections.OrderedDict([('len', stat.st_size), ('name',fn)])
+
+    # print("File: %s" % fn)
+    # print("File length: %d" % filelen)
 
     segmentLen = 10000
 
     # print("Most common bytes: %s" % ' '.join("%02s" % hex(b[0])[2:4] for b in collections.Counter(data[:segmentLen]).most_common(10)))
 
     common = [t[0] for t in collections.Counter(data[:segmentLen]).most_common(10)]
-    print("Most common bytes: %s" % ' '.join("%02x" % chr for chr in common))
+    # print("Most common bytes: %s" % ' '.join("%02x" % chr for chr in common))
+    desc.update(topbytes=["%02x" % chr for chr in common])
 
     # space character
     if 0x40 in common:
@@ -94,27 +99,40 @@ def signatures(fn):
                      (finds[0][0],
                       [j[0] - i[0] for i, j in zip(finds[:-1], finds[1:])]))
 
-    print("upperbitchars of %s at %s" % (set((find[1] for find in finds)), upperbitchars))
+    #print("upperbitchars of %s at %s" % (set((find[1] for find in finds)), upperbitchars))
+    desc.update(upperbitchars=list(set((find[1] for find in finds))))
 
     blocksize = findRecordLen(isobytes[:segmentLen], min(1000, filelen-1))
 
-    print("Block size: %d" % blocksize)
-    print("Blocks: %f" % (filelen / blocksize, ))
+    # print("Block size: %d" % blocksize)
+    # print("Blocks: %f" % (filelen / blocksize, ))
 
     left_over = filelen % blocksize
     if left_over != 0:
-        print("WARNING: %d bytes left over" % left_over)
+        #print("WARNING: %d bytes left over" % left_over)
 
         if left_over == blocksize / 2:   # FIXME: deal better with mistakes from thes odd paired data structures
+            # print("New block size: %d" % blocksize)
+            desc.update(firstblocksize=blocksize)
             blocksize = left_over
-            print("New block size: %d" % blocksize)
+            left_over = filelen % blocksize
+
+    desc.update(blocksize=blocksize)
+    desc.update(blocks=filelen / blocksize)
+    desc.update(left_over=left_over)
 
     # Convert contents of file to a 2-d array of characters, one row per block
     try:
         data_c = np.array(isobytes, 'c')
     except UnicodeEncodeError as e:
-        print(e)
-        return
+        logging.error("badchar in %s: %s" % (fn, e))
+        desc.update(badchar=True)
+        return desc
+
+    if left_over:
+        logging.error("left_over: %d at blocksize %s for %s" % (left_over, blocksize, fn))
+        return desc
+                    
     data_c.shape = (-1, blocksize)
 
     # If we take the positions of decimal places as a signature, there are only 6 unique signatures in dr2129: here is a count for each unique tuple of columns.  E.g. 54 records have a "." in columns 7 and 14.
@@ -124,11 +142,19 @@ def signatures(fn):
     alldpacols = [dpacols(record) for record in data_c]
     dpacol = collections.Counter(alldpacols)
 
-    print("%d unique marker character position signatures. Counts and columns for each:" % len(dpacol))
+    #print("%d unique marker character position signatures. Counts and columns for each:" % len(dpacol))
+    desc.update(num_markers=len(dpacol))
+
     sigs = sorted(list(dpacol.items()), key=lambda t: t[1], reverse=True)
+    desc.update(sigs=sigs[:3])
+
     for t, v in sigs[:10]:
-        print((v, str(t)))
+        #print((v, str(t)))
+        pass
+
     assert(sum(dpacol.values())) == len(data_c)
+
+    return desc
 
     """
     alldpcols = [dpcols(record) for record in data_c]
@@ -138,15 +164,12 @@ def signatures(fn):
     """
 
 if __name__ == "__main__":
-    descs = []
+    print("[")
     for fn in sys.argv[1:]:
-        print 
         if os.path.splitext(fn)[1] in ['.pdf', '.html', '.tar', '.xml', '.txt']:
             continue
 
-        stat = os.stat(fn)
-        desc = collections.OrderedDict([('len', stat.st_size), ('name',fn)])
-        descs.append(desc)
-        signatures(fn)
+        desc = signatures(fn)
+        print(json.dumps(desc) + ",")
 
-    print(json.dumps(descs))
+    print("[]\n]")
